543014bfab933b0b71a5e6d8d8b0c3580cc164a0
billy <abongilebilly990301@gmail.com>
2024-03-28 18:50:54 +0200

    first bean creation

--- src/main/java/com/springbootFirstProgram/dependecy/DependecySpringbootApplication.java
+++ src/main/java/com/springbootFirstProgram/dependecy/DependecySpringbootApplication.java
@@ -2,12 +2,26 @@ package com.springbootFirstProgram.dependecy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DependecySpringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DependecySpringbootApplication.class, args);
+		var runApp = SpringApplication.run(DependecySpringbootApplication.class, args);
+
+		Greeting greeting = runApp.getBean(Greeting.class);
+		System.out.println(greeting.hello());
+
 	}
+
+	// Creating the first bean
+	@Bean
+	public Greeting greeting()
+	{
+		return new Greeting();
+	}
+
+
 
 }
---
+++ src/main/java/com/springbootFirstProgram/dependecy/Greeting.java
@@ -0,0 +1,10 @@
+package com.springbootFirstProgram.dependecy;
+
+//The class is a greetng class---greets the user
+public class Greeting {
+
+    public String hello()
+    {
+        return "Hello Avela";
+    }
+}
