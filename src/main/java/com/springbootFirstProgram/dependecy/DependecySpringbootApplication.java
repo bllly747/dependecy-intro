ca1ffd1e68081585890bc502630a4352c9d7c9f9
billy <abongilebilly990301@gmail.com>
2024-03-28 18:39:07 +0200

    Initial commit

---
+++ src/main/java/com/springbootFirstProgram/dependecy/DependecySpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.springbootFirstProgram.dependecy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependecySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependecySpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootFirstProgram/dependecy/DependecySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootFirstProgram.dependecy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependecySpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
